91e9d49aca847e9002b3e19515ee300662b31492
UIT_21520064 <trthminh1112@gmail.com>
2022-11-18 23:28:05 +0700

    add file snake.py again

--- snake.py
+++ snake.py
@@ -2,20 +2,6 @@ import pygame , sys
 import random
 from pygame.math import Vector2
 
-class Fruit:
-    def __init__(self):
-        self.randomize()
-
-    def draw_fruit(self):
-        fruit_rect = pygame.Rect(self.pos.x * cell_size, self.pos.y * cell_size, cell_size, cell_size)
-
-        pygame.draw.rect(screen, (255, 0, 0), fruit_rect)
-
-    def randomize(self):
-        self.x = random.randint(0, cell_number - 1)
-        self.y = random.randint(0, cell_number - 1)
-        self.pos = Vector2(self.x, self.y)
-
 class Snake:
     def __init__(self):
         self.body = [Vector2(5, 10), Vector2(6, 10), Vector2(7, 10)]
@@ -113,7 +99,6 @@ cell_number = 20
 screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
 clock = pygame.time.Clock()
 
-fruit = Fruit()
 snake = Snake()
 
 SCREEN_UPDATE = pygame.USEREVENT
@@ -146,8 +131,5 @@ while True:
 
     screen.fill((214, 150, 187))
     snake.draw_snake()
-    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
-
-# https://www.youtube.com/watch?v=QFvqStqPCRU
